2005ddf737770e95c2dfead9cb3c341e0dce4efe
Mireia Ribera <ribera@ub.edu>
2018-02-13 09:55:44 +0100

    Add files via upload

---
+++ demo/myscript.js
@@ -0,0 +1,19 @@
+alert("Hello , world");
+var number = 5;
+var numbers = [5,10,15];
+var fruit = {
+	kind: "grape",
+	color:"green",
+	quantity:12,
+	tasty: true
+};
+console.log("hello");
+console.log(number);
+console.log(numbers[0]);
+console.log(fruit.kind);
+
+var calculateVAT = function(price) {return (price * 0.21);};
+
+
+vat = calculateVAT(100);
+console.log(100+vat);
---
+++ demo/myscript2.js
@@ -0,0 +1,7 @@
+var jsonFruit = {
+	"kind": "grape",
+	"color":"green",
+	"quantity":12,
+	"tasty": true
+};
+console.log(jsonFruit.kind);
